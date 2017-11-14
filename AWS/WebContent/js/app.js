71fa0961312839346a25c1b00f9cac95b0f650fd
vignesh <vignesh.kumaran@cognizant.com>
2017-11-14 16:45:28 +0530

    date picker changes and highlight changes

--- AWS/WebContent/js/app.js
+++ AWS/WebContent/js/app.js
@@ -3,6 +3,7 @@ var app = angular.module("aws", ['ui.router','ui.bootstrap','ngTable']);
 app.config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/dashboard');
+    
     $stateProvider
         .state('dashboard', {
             url: '/dashboard',
@@ -19,17 +20,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 });
 
-app.controller("awsCtrl", ['$scope','$state', '$http','NgTableParams',function($scope,$state, $http,NgTableParams) {
+app.controller("awsCtrl", ['$scope','$state', '$http','NgTableParams',function($scope,$state,$http,NgTableParams) {
 	
 	/*$scope.$state = $state;*/
 	
-	var vm=this;
-	
+	var vm=this;	
 	
 	vm.products = "Basic_products";
+	
+	vm.dbcondition=false;
+	vm.upcondition=false;
        
     $http.get("json/Information.json").then(function (response) {
     	vm.data = response.data;
+    	vm.datalength = vm.data.length;
     },function (error){
         console.log(error, 'can not get data.');
     });
@@ -51,16 +55,43 @@ app.controller("awsCtrl", ['$scope','$state', '$http','NgTableParams',function($
           }];
     
 	vm.highlight = function(label){
-    	for(var i=0;i<content.length;i++) {
-    		if(content[i].label==label)
+    	for(var i=0;i<vm.content.length;i++) {
+    		if(vm.content[i].label==label)
     			{
-    			content[i].isTrue=true;
+    			vm.content[i].isTrue=true;
     			}
     		else
     			{
-    			content[i].isTrue=false;
+    			vm.content[i].isTrue=false;
     			}
     	}
     }
+	
+	vm.dbconditionfunc = function(){
+		vm.dbcondition=true;
+		vm.upcondition=false;
+	}
+	vm.upconditionfunc = function(){
+		vm.dbcondition=false;
+		vm.upcondition=true;
+	}
+
+	vm.dateOptions = {
+			    formatYear: 'yy',
+			    maxDate: new Date(2100, 5, 22),
+			    minDate: new Date(),
+			    startingDay: 1
+			  };
+
+			  vm.open = function() {
+			    vm.popup.opened = true;
+			  };
+
+			  vm.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+			  vm.format = vm.formats[0];
+
+			  vm.popup = {
+			    opened: false
+			  };
 		  
 }]);
